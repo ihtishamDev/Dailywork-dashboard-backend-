e3e21c49420e6d08891fc3d8ca54d1d50f364f18
ihtishamTech <shamijhn151@gmail.com>
2026-02-18 06:45:01 +0500

    database updated

--- app/db/database.py
+++ app/db/database.py
@@ -1,11 +1,23 @@
+# import mysql.connector
+
+# def get_connection():
+
+#     conn = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="Ihtisham@123",
+#         database="dailytask"
+#     )
+#     return conn
+
+import os
 import mysql.connector
 
 def get_connection():
-
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Ihtisham@123",
-        database="dailytask"
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
-    return conn
