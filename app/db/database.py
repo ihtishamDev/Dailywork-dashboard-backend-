74551844812f56b00512c66b2b7eefd77e3ac5a5
ihtishamTech <shamijhn151@gmail.com>
2026-02-18 08:41:06 +0500

    aiven connection database

--- app/db/database.py
+++ app/db/database.py
@@ -1,15 +1,3 @@
-# import mysql.connector
-
-# def get_connection():
-
-#     conn = mysql.connector.connect(
-#         host="localhost",
-#         user="root",
-#         password="Ihtisham@123",
-#         database="dailytask"
-#     )
-#     return conn
-
 import os
 import mysql.connector
 
